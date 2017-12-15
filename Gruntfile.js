0df071cb6be53f50b88df74f772acfa7f95d5cae
谭谈 <tantan@wxchina.com>
2017-12-15 16:23:42 +0800

    页面框架搭建，页面路由处理

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,32 +3,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
      //读取package.json文件
     pkg: grunt.file.readJSON('package.json'),
-    //concat用来合并js文件
-    concat: {
-      options: {
-        // 定义一个用于插入合并输出文件之间的字符
-        separator: ';'
-      },
-      dist: {
-        // 将要被合并的文件
-        src: ['app/src/**/*.js'],
-        // 合并后的JS文件的存放位置
-        dest: 'app/dist/<%= pkg.name %>.js'
-      }
-    },
-     //uglify用来压缩js文件
-    uglify: {
-      options: {
-        // 此处定义的banner注释将插入到输出文件的顶部
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          //uglify会自动压缩concat任务中生成的文件
-          'app/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-     },
     //jshint用来检查js代码规范
     jshint: { 
       files: ['Gruntfile.js', 'app/src/**/*.js'], //要进行js检查的文件
@@ -59,12 +33,6 @@ module.exports = function(grunt) {
         ]
       }
     },
-    //wiredep自动将bower_components中的包加入到index.html中
-    wiredep: {
-      task: {
-        src: ['app/index.html']
-      }
-    },
     //服务器连接服务
     connect: {  
         options: {
@@ -84,12 +52,7 @@ module.exports = function(grunt) {
     }
   });
 
-  //grunt.loadNpmTasks('grunt-contrib-concat');
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  //grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.registerTask('serve', ['connect','watch']);
-
 };
